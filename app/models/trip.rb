2927eb68837aa2d7f6d87073a384470da0a82621
Hasan Zia <hasanzia@live.com>
2018-04-27 22:37:01 +0500

    Optimize query and code readability in Trip to find same user and vehicle trips with a scope

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -35,22 +35,26 @@ class Trip < ApplicationRecord
   validate :completed_trip_should_not_overlap_other_trip_of_same_user_in_same_time, if: :ending_at
   validate :completed_trip_should_not_overlap_other_trip_of_same_vehicle_in_same_time, if: :ending_at
 
+  scope :all_except, ->(trip) { where.not(id: trip) }
+
   def completed_trip_should_have_duration
     errors.add(:ending_at, "cannot be smaller or equal to Starting at of a Trip") if ending_at <= starting_at
   end
 
   def completed_trip_should_not_overlap_other_trip_of_same_user_in_same_time
-    if Trip.where.not(id: self.id).where(user_id: self.user_id).where(" ('#{self.starting_at}' BETWEEN starting_at AND ending_at) OR ( '#{self.ending_at}' BETWEEN starting_at AND ending_at) ").any?
+    other_trips_of_same_user = Trip.all_except(self).where(user_id: self.user_id)
+    if other_trips_of_same_user.where(" ('#{self.starting_at}' BETWEEN starting_at AND ending_at) OR ( '#{self.ending_at}' BETWEEN starting_at AND ending_at) ").any?
       errors.add(:user_id, "user can't overlap older trip of same user in same time")
-    elsif Trip.where.not(id: self.id).where(user_id: self.user_id).where(" (starting_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') OR (ending_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') ").any?
+    elsif other_trips_of_same_user.where(" (starting_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') OR (ending_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') ").any?
       errors.add(:user_id, "user can't overlap older trip of same user in same time")
     end
   end
 
   def completed_trip_should_not_overlap_other_trip_of_same_vehicle_in_same_time
-    if Trip.where.not(id: self.id).where(vehicle_id: self.vehicle_id).where(" ('#{self.starting_at}' BETWEEN starting_at AND ending_at) OR ( '#{self.ending_at}' BETWEEN starting_at AND ending_at) ").any?
+    other_trips_of_same_vehicle = Trip.all_except(self).where(vehicle_id: self.vehicle_id)
+    if other_trips_of_same_vehicle.where(" ('#{self.starting_at}' BETWEEN starting_at AND ending_at) OR ( '#{self.ending_at}' BETWEEN starting_at AND ending_at) ").any?
       errors.add(:vehicle_id, "vehicle can't overlap older trip of same vehicle in same time")
-    elsif Trip.where.not(id: self.id).where(vehicle_id: self.vehicle_id).where(" (starting_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') OR (ending_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') ").any?
+    elsif other_trips_of_same_vehicle.where(" (starting_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') OR (ending_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') ").any?
       errors.add(:vehicle_id, "vehicle can't overlap older trip of same vehicle in same time")
     end
   end
