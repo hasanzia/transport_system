5338d19d12a5a2517b6a6f7de681a3ef24cdb33d
Hasan Zia <hasanzia@live.com>
2018-04-27 21:57:39 +0500

    Add validations for same user and same vehicle in a trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -31,4 +31,28 @@ class Trip < ApplicationRecord
   belongs_to :vehicle
 
   validates :starting_at, presence: true
+  validate :completed_trip_should_have_duration, if: :ending_at
+  validate :completed_trip_should_not_overlap_other_trip_of_same_user_in_same_time, if: :ending_at
+  validate :completed_trip_should_not_overlap_other_trip_of_same_vehicle_in_same_time, if: :ending_at
+
+  def completed_trip_should_have_duration
+    errors.add(:ending_at, "cannot be smaller or equal to Starting at of a Trip") if ending_at <= starting_at
+  end
+
+  def completed_trip_should_not_overlap_other_trip_of_same_user_in_same_time
+    if Trip.where.not(id: self.id).where(user_id: self.user_id).where(" ('#{self.starting_at}' BETWEEN starting_at AND ending_at) OR ( '#{self.ending_at}' BETWEEN starting_at AND ending_at) ").any?
+      errors.add(:user_id, "user can't overlap older trip of same user in same time")
+    elsif Trip.where.not(id: self.id).where(user_id: self.user_id).where(" (starting_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') OR (ending_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') ").any?
+      errors.add(:user_id, "user can't overlap older trip of same user in same time")
+    end
+  end
+
+  def completed_trip_should_not_overlap_other_trip_of_same_vehicle_in_same_time
+    if Trip.where.not(id: self.id).where(vehicle_id: self.vehicle_id).where(" ('#{self.starting_at}' BETWEEN starting_at AND ending_at) OR ( '#{self.ending_at}' BETWEEN starting_at AND ending_at) ").any?
+      errors.add(:vehicle_id, "vehicle can't overlap older trip of same vehicle in same time")
+    elsif Trip.where.not(id: self.id).where(vehicle_id: self.vehicle_id).where(" (starting_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') OR (ending_at BETWEEN '#{self.starting_at}' AND '#{self.ending_at}') ").any?
+      errors.add(:vehicle_id, "vehicle can't overlap older trip of same vehicle in same time")
+    end
+  end
+
 end
